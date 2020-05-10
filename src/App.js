2fcdbd2b72314e7deb50245b39d96a50c3a8bda8
Emma Schreifels <emma.schreifels@gmail.com>
2020-05-10 16:57:19 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo_logo.svg';
 import './App.css';
+import Menu from './components/Menu';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
+        <Menu/>
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          emma schreifels
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          --&#62; i curiously create &#60;--
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );

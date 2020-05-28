7a131e781913a134fb5cfeef2fccf161f97ade3e
Emma Schreifels <emma.schreifels@gmail.com>
2020-05-27 21:02:44 -0500

    Updates

---
+++ precache-manifest.7cbfda6d4d5ec74cd9d946db4ac4ef42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "154d0cd24b2bc967ea05454d56655bd1",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "678e556c0879610fa51a",
+    "url": "/hello/static/css/main.a6494cf8.chunk.css"
+  },
+  {
+    "revision": "94df17358082cfa53e4f",
+    "url": "/hello/static/js/2.818664c5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/hello/static/js/2.818664c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "678e556c0879610fa51a",
+    "url": "/hello/static/js/main.582a86ce.chunk.js"
+  },
+  {
+    "revision": "3f2a1ca307fbf204363f",
+    "url": "/hello/static/js/runtime-main.c6379c10.js"
+  },
+  {
+    "revision": "d2c0ef1e15236b65649b2abeee8fe73c",
+    "url": "/hello/static/media/logo_logo.d2c0ef1e.svg"
+  }
+]);
